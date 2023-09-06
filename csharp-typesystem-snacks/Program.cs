0b96261a6c919f449235f8d12ed56aab327d97d3
LucaZanfrisco <lzanfrisco9@gmail.com>
2023-09-06 14:29:01 +0200

    Modifica tipo dato snack 1 e svolgimento snack 3

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -4,10 +4,10 @@
 // L’utente inserisce due numeri in successione. Il software stampa il maggiore.
 
 Console.Write("Inserire il primo numero: ");
-int primoNumero = int.Parse(Console.ReadLine());
+float primoNumero = float.Parse(Console.ReadLine());
 
 Console.Write("Inserie il secondo numero: ");
-int secondoNumero = int.Parse(Console.ReadLine());
+float secondoNumero = float.Parse(Console.ReadLine());
 
 if(primoNumero > secondoNumero)
 {
@@ -37,4 +37,20 @@ else
     Console.WriteLine($"La seconda parola '{secondaParola}' è la piu lunga");
 }
 
+Console.WriteLine("\n------------------------\n");
 
+// Snack 3
+// Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+int i = 1;
+float somma = 0;
+while(i <= 10)
+{
+    Console.Write($"Inserire il numero {i} :");
+    somma += float.Parse(Console.ReadLine());
+    i++;
+}
+
+Console.WriteLine($"La somma di tutti i numeri inseriti è: {somma}");
+
+Console.WriteLine("\n------------------------\n");
